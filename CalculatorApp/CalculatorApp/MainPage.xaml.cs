63bddcf7d2a5993ada08d820727af82b3090bbbb
Bart1251 <bartlomiej.wiecek@vp.pl>
2023-12-12 13:54:55 +0100

    Back Finished

--- CalculatorApp/CalculatorApp/MainPage.xaml.cs
+++ CalculatorApp/CalculatorApp/MainPage.xaml.cs
@@ -22,10 +22,12 @@ namespace CalculatorApp
 
     public partial class MainPage : ContentPage
     {
-        private double a;
-        private double b;
+        private double a = 0;
+        private double b = 0;
         private Operation operation = Operation.None;
-
+        private bool comma = false;
+        private double result = 0;
+        private bool equalsClicked = false;
 
         public MainPage()
         {
@@ -36,20 +38,163 @@ namespace CalculatorApp
         {
             if(operation == Operation.None)
             {
-                a = double.Parse(a.ToString() + ((Button)sender).Text);
+                if(comma)
+                {
+                    a = double.Parse(a.ToString() + "." + ((Button)sender).Text);
+                    comma = false;
+                }
+                else
+                    a = double.Parse(a.ToString() + ((Button)sender).Text);
             }
             else
             {
-                b = double.Parse(b.ToString() + ((Button)sender).Text);
+                if (comma)
+                {
+                    b = double.Parse(b.ToString() + "." + ((Button)sender).Text);
+                    comma = false;
+                }
+                else
+                    b = double.Parse(b.ToString() + ((Button)sender).Text);
             }
         }
 
         private void OperationClick(object sender, EventArgs e)
         {
-            switch(((Button)sender).Text) 
-            { 
-
+            switch(((Button)sender).Text)
+            {
+                case "+":
+                    operation = Operation.Add;
+                    break;
+                case "-":
+                    operation = Operation.Subtract;
+                    break;
+                case "*":
+                    operation = Operation.Multiply;
+                    break;
+                case "/":
+                    operation = Operation.Divide;
+                    break;
+                case "x^2":
+                    result = a * a;
+                    operation = Operation.Square;
+                    Print();
+                    break;
+                case "sqrt":
+                    result = Math.Sqrt(a);
+                    operation = Operation.SquareRoot;
+                    Print();
+                    break;
+                case "1/x":
+                    result = 1 / a;
+                    operation = Operation.Inverse;
+                    Print();
+                    break;
+                default:
+                    throw new ArgumentException();
             }
+        }
+
+        private void Equals(object sender, EventArgs e)
+        {
+            switch(operation)
+            {
+                case Operation.Add:
+                    result = a + b;
+                    equalsClicked = true;
+                    Print();
+                    break;
+                case Operation.Subtract:
+                    result = a - b;
+                    equalsClicked = true;
+                    Print();
+                    break;
+                case Operation.Multiply:
+                    result = a * b;
+                    equalsClicked = true;
+                    Print();
+                    break;
+                case Operation.Divide:
+                    result = a / b;
+                    equalsClicked = true;
+                    Print();
+                    break;
+            }
+        }
+
+        private void Print()
+        {
+            if(operation == Operation.None)
+            {
+                Output_Label.Text = a.ToString();
+                OutputEquation_Label.Text = "";
+                return;
+            }
+            switch(operation)
+            {
+                case Operation.Add:
+                case Operation.Subtract:
+                case Operation.Multiply:
+                case Operation.Divide:
+                    if(equalsClicked)
+                    {
+                        Output_Label.Text = result.ToString();
+                        OutputEquation_Label.Text = a.ToString() + " " + OperationToSign(operation) + " " + b.ToString() + " =";
+                    }
+                    else
+                    {
+                        Output_Label.Text = b.ToString();
+                        OutputEquation_Label.Text = a.ToString() + " " + OperationToSign(operation);
+                    }
+                    break;
+                case Operation.Square:
+                    Output_Label.Text = result;
+                    OutputEquation_Label.Text = "sqr(" + a + ")";
+                    break;
+                case Operation.SquareRoot:
+                    Output_Label.Text = result;
+                    OutputEquation_Label.Text = "sqrt(" + a + ")";
+                    break;
+                case Operation.Inverse:
+                    Output_Label.Text = result;
+                    OutputEquation_Label.Text = "1/" + a.ToString();
+                    break;
+            }
+        }
+
+        private string OperationToSign(Operation operation)
+        {
+            string operationSign = "";
+            switch (operation)
+            {
+                case Operation.Add:
+                    operationSign = "+";
+                    break;
+                case Operation.Subtract:
+                    operationSign = "-";
+                    break;
+                case Operation.Multiply:
+                    operationSign = "*";
+                    break;
+                case Operation.Divide:
+                    operationSign = "/";
+                    break;
+            }
+            return operationSign;
+        }
+
+        private void ClearClick(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            result = 0;
+            comma = false;
+            operation = Operation.None;
+            equalsClicked = false;
+        }
+            
+        private void CommaClick(object sender, EventArgs e)
+        {
+            comma = true;
         }
 
     }
