ed473a30b4d4cd9356988844fc64f94110185cf3
Bart1251 <bartlomiej.wiecek@vp.pl>
2023-12-12 12:53:17 +0100

    commit

--- CalculatorApp/CalculatorApp/MainPage.xaml.cs
+++ CalculatorApp/CalculatorApp/MainPage.xaml.cs
@@ -8,11 +8,49 @@ using Xamarin.Forms;
 
 namespace CalculatorApp
 {
+    public enum Operation
+    {
+        None,
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Square,
+        SquareRoot,
+        Inverse
+    }
+
     public partial class MainPage : ContentPage
     {
+        private double a;
+        private double b;
+        private Operation operation = Operation.None;
+
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void NumberClick(object sender, EventArgs e)
+        {
+            if(operation == Operation.None)
+            {
+                a = double.Parse(a.ToString() + ((Button)sender).Text);
+            }
+            else
+            {
+                b = double.Parse(b.ToString() + ((Button)sender).Text);
+            }
+        }
+
+        private void OperationClick(object sender, EventArgs e)
+        {
+            switch(((Button)sender).Text) 
+            { 
+
+            }
+        }
+
     }
 }
