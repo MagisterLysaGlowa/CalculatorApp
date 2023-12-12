5b7de2e7fad969a6b1b20da871a1216ebd5bb0ce
MagisterLysaGlowa <kacperpiaskowy937@gmail.com>
2023-12-12 14:05:02 +0100

    Dodanie nowych elementów UI

--- CalculatorApp/CalculatorApp/Views/MainPage.xaml.cs
+++ CalculatorApp/CalculatorApp/Views/MainPage.xaml.cs
@@ -10,9 +10,57 @@ namespace CalculatorApp
 {
     public partial class MainPage : ContentPage
     {
+        private double width = 0;
+        private double height = 0;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OperationClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumberClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EqualsClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CommaClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearClick(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); //must be called
+            if (this.width != width || this.height != height)
+            {
+                this.width = width;
+                this.height = height;
+                if(this.width > this.height)
+                {
+                    GridLayout.Children.Remove(VerticalGridControll);
+                    GridLayout.Children.Add(HorizontalGridControll);
+                    DisplayAlert("teraz", "teraz", "teraz");
+                }
+                else
+                {
+                    GridLayout.Children.Remove(HorizontalGridControll);
+                    GridLayout.Children.Add(VerticalGridControll);
+                }
+            }
         }
     }
 }
